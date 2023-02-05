894fea744d1f591f29f95e82e6044b553266e714
Noor <noor2159805@gmail.com>
2023-02-05 12:44:05 +0200

    using fetch

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import '../App.css';
 
-const Table = ({names}) => {
+const Table = () => {
 
-    const users = names.map(({id,first_name,last_name,avatar,email}) => {
+    const [state, setState] = useState([])
+
+    const fetchData = () => {
+        fetch("https://reqres.in/api/users?page=1")
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            setState(data.data)
+          })
+      }
+    
+      useEffect(() => {
+        fetchData()
+      }, [])
+
+
+    const users = state.map(({id,first_name,last_name,avatar,email}) => {
         return (
           <tr key={id}>
             <td>{id}</td>
